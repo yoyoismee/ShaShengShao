745c2b3b8a725b47a941d47e025aa10fc4c2618e
Thanawit Prasongpongchai <taepras@gmail.com>
2016-12-11 13:04:22 +0700

    organize config file

--- inc/Config.hpp
+++ inc/Config.hpp
@@ -1,6 +1,9 @@
-﻿#define DEBUG
+﻿// -------------------- debugs --------------------
+#define DEBUG
 //#define COMPARE
 
+
+// -------------------- paths --------------------
 #define HITALERT_BASE_PATH "../../../hitalert/"
 #define HITALERT_RUNTIME_PATH "" //HITALERT_BASE_PATH
 
@@ -11,17 +14,33 @@
 #define SOUND_R_LOW HITALERT_RUNTIME_PATH "res/RightLow.wav"
 #define SOUND_L_LOW HITALERT_RUNTIME_PATH "res/LeftLow.wav"
 
+
+// -------------------- input --------------------
+#define USE_WEBCAM
+#define CAP_CAM_NO 0
+#define CAP_VID_PATH HITALERT_BASE_PATH "test_data/"
+#define CAP_VID_NAME "35.mp4"
+
+
+// -------------------- output --------------------
+//#define OUTPUT_VIDEO
+#define OUTPUT_VIDEO_PATH HITALERT_BASE_PATH "output/out.mp4"
+
 //#define ALERT_LEFT_RIGHT 
 //#define ALERT_HIGH_ONLY
 
-#define PROCESS_WIDTH 480//720//480//720//640 // 400
-#define PROCESS_HEIGHT 360//480//360//270//480//480// 360 // 225
+
+// -------------------- image config --------------------
+#define PROCESS_WIDTH 480//720
+#define PROCESS_HEIGHT 360//480
 #define FRAMES_SKIP 0
 
 #define ROI_COUNT 3
 #define ROI_WIDTH_REL 0.36
 // calculated from human dimension of 60 cm in width, 50cm distance with 60 deg FOV camera
 
+
+// -------------------- hitalert parameters --------------------
 #define TRACK_CORNERS 150
 #define TRACK_QUALITY 0.05
 #define TRACK_MIN_DIST 20
@@ -36,16 +55,9 @@
 #define DIST_RATIO_RANGE_THRESH 0.03
 #define DIST_RATIO_COEFF 100
 
-#define USE_WEBCAM
-#define CAP_CAM_NO 0
-#define CAP_VID_PATH HITALERT_BASE_PATH "test_data/"
-#define CAP_VID_NAME "35.mp4"
-
-//#define OUTPUT_VIDEO
-#define OUTPUT_VIDEO_PATH HITALERT_BASE_PATH "output/out.mp4"
 
 
-////////// derived configs //////////
+//////////////////// derived configs ////////////////////
 
 #ifdef USE_WEBCAM
 #define CAP_SRC 0
